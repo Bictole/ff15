e66db617057fb7913f5f9059172093c666725d0a
Bictole <>
2023-03-05 18:42:53 +0000

    feat(emote): More emotes

--- addEmote.js
+++ addEmote.js
@@ -1,9 +1,24 @@
 function addEmote(name, message, msg) {
   if (name.includes("bictole")) {
-      msg.react("503691113123545093");
+    msg.react("428533050310262791");
+  }
+  else if (name.includes("marius")) {
+    msg.react("1064287860024422440");
+  }
+  else if (name.includes("rehoon")) {
+    msg.react("356541796832247818");
+  }
+  else if (name.includes("robert chassiet")) {
+    msg.react("428589910228992024");
+  }
+  else if (name.includes("taylord12")) {
+    msg.react("428592006260981780");
+  }
+  else if (name.includes("euphorbe")) {
+    msg.react("428536247330537473");
   }
   else {
-    msg.react("428533050310262791")
+    msg.react("1063815411289837648")
   }
 }
 
