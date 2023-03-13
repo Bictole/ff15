6ca8cf114800a9ecf115cdf5cca689930c39acdf
Victor Simonin <victor.simonin@epita.fr>
2023-03-14 00:54:40 +0100

    fix(nix): Broken openssh + improve log

--- replit.nix
+++ replit.nix
@@ -1,7 +1,6 @@
 { pkgs }: {
 	deps = [
-		pkgs.openssh_with_kerberos
-  pkgs.nodejs-18_x
+    pkgs.nodejs-18_x
     pkgs.nodePackages.typescript-language-server
     pkgs.yarn
     pkgs.replitPackages.jest
