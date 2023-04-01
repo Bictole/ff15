b7fd02271cce248fec35e56152d92848edf873a4
Victor Simonin <victor.simonin@epita.fr>
2023-04-01 23:54:51 +0200

    Add sincos timer + database entry

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const keepAlive = require("./server")
 const { addEmote } = require('./emote')
 const { kivaou, kivaouTryhard } = require('./kivaou')
 
+var fs = require('fs');
+const DateDiff = require("date-diff").default;
+
+const Database = require("@replit/database")
+
+const db = new Database();
+
+db.get("sincos_timer").then(sincos_timer => {
+  if (!sincos_timer) {
+    let date = new Date();
+    console.log(`Sincos date : ${date}`)
+    db.set("sincos_timer", date);
+  }
+})
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -26,17 +41,47 @@ client.on("messageCreate", (message) => {
     message.reply(({
       content: "probably a noice idea ...",
     }))
-    console.log(`[FF15] by ${name}`);
+    console.log(`INFO [FF15] by ${name}`);
   }
 
   if (message.content === "kivaou") {
     kivaou(message);
-    console.log(`[KIVAOU] by ${name}`);
+    console.log(`INFO [KIVAOU] by ${name}`);
   }
 
   if (message.content === "kivaou tryhard") {
     kivaouTryhard(message);
-    console.log(`[KIVAOUTRYHARD] by ${name}`)
+    console.log(`INFO [KIVAOUTRYHARD] by ${name}`)
+  }
+
+  if (message.content.includes("sin") || message.content.includes("cos")) {
+    if (message.content === "sincos") {
+      db.get("sincos_timer").then(sincos_timer => {
+        let lastDate = new Date(sincos_timer);
+         
+        message.reply(({
+        content: `Last usage of sin or cos : ${lastDate.toUTCString()}`,
+        }))
+      })
+
+      console.log(`INFO [sincos] by ${name}`);
+    }
+    else {
+      db.get("sincos_timer").then(sincos_timer => {
+        let now = new Date();
+        let lastDate = new Date(sincos_timer);
+
+        let diff = new DateDiff(now, lastDate);
+                
+        message.reply(({
+        content: `SIN OR COS USAGE ! YOU HOLD ${diff.hours()} HOURS ! TIMER RESET !`,
+        }));
+
+        db.set("sincos_timer", now)
+      })
+
+      console.log(`INFO [sincos] SinCos RESET`);
+    }
   }
 
   if (Math.random() < 0.10) {
